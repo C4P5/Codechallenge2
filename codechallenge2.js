2606a01b300a68574101cd62de3418a2f970a1f4
CAPS <caprioli.santiago@gmail.com>
2023-09-19 21:19:33 -0700

    Edit curson on image sigue faltando CORS

--- codechallenge2.js
+++ codechallenge2.js
@@ -3,45 +3,41 @@ function updateList() {
         .then(response => response.json())
         .then(data => {
             let listContainer = document.querySelector('.Lista');
-            // Clear the existing list
+
             listContainer.innerHTML = '';
             
-            // Loop through the data and create list items
             data.forEach(item => {
                 let listItem = document.createElement('div');
                 listItem.textContent = `Nombre: ${item.nombre}, Apellido: ${item.apellido}, Grupo: ${item.grupo}, Sala: ${item.sala}`;
-                // Create an image for deleting the item
                 let deleteIcon = document.createElement('img');
-                deleteIcon.src = 'checkbox-152188_640.png'; // Replace with your delete icon URL
+                deleteIcon.src = 'checkbox-152188_640.png'; 
                 deleteIcon.alt = 'Delete';
                 deleteIcon.width = 130;
                 deleteIcon.height = 105;
-                
-                // Add a click event listener to delete the item
+      
+        // pointer img //
+            deleteIcon.addEventListener('mouseover', () => {
+            deleteIcon.style.cursor = 'pointer';
+            });
+
+            deleteIcon.addEventListener('mouseout', () => {
+            deleteIcon.style.cursor = 'auto';
+            });
+
                 deleteIcon.addEventListener('click', () => {
-                    // Send a DELETE request to your API to delete the item
                     fetch(`https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/grupo255`, {
                         method: 'DELETE'
                     })
                     .then(() => {
-                        // After successful deletion, remove the item from the list
                         listItem.remove();
                     })
                     .catch(error => console.error('Error deleting item:', error));
                 });
-
-                // Append the delete icon to the list item
                 listItem.appendChild(deleteIcon);
-                
-                // Append the list item to the list container
-                listContainer.appendChild(listItem);
+                 listContainer.appendChild(listItem);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
 }
-
-// Call the updateList function every 1.5 seconds
 setInterval(updateList, 1500);
-
-// Initial call to populate the list
 updateList();
